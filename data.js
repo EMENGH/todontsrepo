6349867b0f234cb9defe5e222ef5bb57211d5aa6
kgreen14 <kgreen14@gmail.com>
2017-07-21 14:37:56 -0400

    fresh server template/boilerplate using nodde, just clone and npm install to get all dependencies

---
+++ data.js
@@ -0,0 +1,11 @@
+module.exports = {
+  seededToDont: [
+    {
+      description: "shove old ladies",
+      urgent: true
+    }, {
+      description: "rob a bank",
+      urgent: false
+    }
+  ]
+};
---
+++ server.js
@@ -0,0 +1,42 @@
+/* packages */
+var path        = require('path');
+var logger      = require('morgan');
+var express     = require('express');
+var hbs         = require('hbs');
+/* app settings*/
+var app         = express();
+var port        = process.env.PORT || 3000;
+/* set up the application params*/
+
+// log
+app.use( logger('dev'));
+
+/*Views*/
+app.set('view engine', 'hbs');
+
+/* HOME */
+app.get('/', function(req,res) {
+  res.send('This is our Home Page');
+});
+
+/* INDEX TODOS */
+app.get('/todont', function(req,res) {
+  var seededTodont = [
+    {
+      description: "get beer",
+      urgent: true
+    }, {
+      description: "dry cleaning",
+      urgent: false
+    }
+  ];
+
+  res.render('todont/index', {
+    todos: seededTodont
+  });
+});
+
+// Start server
+app.listen(port, function() {
+  console.info('Server Up -- Ready to serve hot todos on port', port,"//", new Date());
+});
